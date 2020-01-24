119b7e9cf2cae5b2a0916d4267cb4a95aea24bd1
jung-jin-lee <jungjin.lee.dev@gmail.com>
2020-01-25 01:29:22 +0900

    Updates

---
+++ precache-manifest.a2de955e5131a73167dbb9073ed93b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25d924dc823f1b3d5d81d1b097e315e",
+    "url": "/facebook-comment/index.html"
+  },
+  {
+    "revision": "2a83f10d4bc8c63c26bc",
+    "url": "/facebook-comment/static/css/main.acc7285a.chunk.css"
+  },
+  {
+    "revision": "b862ba81a7578dc02d45",
+    "url": "/facebook-comment/static/js/2.2652d8f6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/facebook-comment/static/js/2.2652d8f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a83f10d4bc8c63c26bc",
+    "url": "/facebook-comment/static/js/main.39a08d5d.chunk.js"
+  },
+  {
+    "revision": "eb0a6e3e074d7345a11b",
+    "url": "/facebook-comment/static/js/runtime-main.49a5f444.js"
+  }
+]);
